d80a73a1c7537b5917c770736f59333e62f479d1
Helen Hood <helen.c.hood@gmail.com>
2015-02-08 18:39:01 -0500

    Seed exercises

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+exercise_attrs = [
+  { name: 'Atmosphere', description: 'Imagine you are in a cathedral.' },
+  { name: 'Centers', description: 'Identify and move from a particular center.' },
+  { name: 'Repetiion', description: 'Listen to your partner and repeat what they say.' }
+]
+
+exercise_attrs.each do |attrs|
+  exercise = Exercise.find_by(attrs)
+
+  if exercise.present?
+    puts "Exercise '#{attrs[:name]}' already in the database."
+  else
+    Exercise.create(attrs)
+    puts "Exercise '#{attrs[:name]}' created."
+  end
+end
