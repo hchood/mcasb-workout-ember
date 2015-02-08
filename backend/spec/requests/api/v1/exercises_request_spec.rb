faa93ccce041650154782fa70021b88c9487f90e
Helen Hood <helen.c.hood@gmail.com>
2015-02-08 17:26:09 -0500

    Add json-schema matcher & expectation in exercises#index test
    Is there a way to test the ordering of objects using JSON schema?
    Would be nice to get rid of the be_json_eq expectation.

--- backend/spec/requests/api/v1/exercises_request_spec.rb
+++ backend/spec/requests/api/v1/exercises_request_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 require 'api/v1/exercises_controller'
 
-describe "Exercises requests" do
-  describe "GET #index" do
-    it "displays all exercises, ordered by name" do
-      last_exercise = create(:exercise, name: "Repetiion")
-      first_exercise = create(:exercise, name: "Centers")
-      middle_exercise = create(:exercise, name: "Imagination Box")
+describe 'Exercises requests' do
+  describe 'GET #index' do
+    it 'displays all exercises, ordered by name' do
+      last_exercise = create(:exercise, name: 'Repetiion')
+      first_exercise = create(:exercise, name: 'Centers')
+      middle_exercise = create(:exercise, name: 'Imagination Box')
 
       ordered_exercises = [first_exercise, middle_exercise, last_exercise]
       serialized_exercises = ActiveModel::ArraySerializer.new(ordered_exercises,
@@ -16,6 +16,7 @@ describe "Exercises requests" do
 
       expect(response.status).to eq 200
       expect(json).to be_json_eq(serialized_exercises)
+      expect(response).to match_response_schema('exercises')
     end
   end
 end
